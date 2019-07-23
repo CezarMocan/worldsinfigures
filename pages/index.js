63836ce3984b6efecdd710ccfe0664ad174e9588
Cezar Mocan <mocancezar@gmail.com>
2019-07-23 12:33:33 +0200

    canvas resize

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,6 @@ export default class Index extends React.Component {
     constructor(props) {
         super(props)
         this.onImageLoad = this.onImageLoad.bind(this)
-        this._canvasWidth = 500
-        this._canvasHeight = 400
         this.state = {
             scale: 16,
             rotateX: 0,
@@ -30,37 +28,43 @@ export default class Index extends React.Component {
             translateX: 0,
             translateY: 0,
             // projection: 'geoPierceQuincuncial'
-            projection: 'geoEquirectangular'
+            projection: 'geoEquirectangular',
+            canvasWidth: 700,
+            canvasHeight: 400
         }
     }
     get canvasContext() {
         return this._canvas.getContext('2d')
+    }
+    get canvasWidth() {
+        return this.state.canvasWidth
+    }
+    get canvasHeight() {
+        return this.state.canvasHeight
     }
     renderMap() {
         const dx = this._image.width
         const dy = this._image.height
 
         if (!this.sourceData) {
-            this.canvasContext.clearRect(0, 0, this._canvasWidth, this._canvasHeight);
+            this.canvasContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
             this.canvasContext.save()
-            this.canvasContext.drawImage(this._image, 0, 0, dx, dy, 0, 0, this._canvasWidth, this._canvasHeight)
+            this.canvasContext.drawImage(this._image, 0, 0, dx, dy, 0, 0, this.canvasWidth, this.canvasHeight)
             this.canvasContext.restore()
-            this.sourceData = this.canvasContext.getImageData(0, 0, this._canvasWidth, this._canvasHeight).data
+            this.sourceData = this.canvasContext.getImageData(0, 0, this.canvasWidth, this.canvasHeight).data
         }
 
-        this.target = this.canvasContext.createImageData(this._canvasWidth, this._canvasHeight)
+        this.target = this.canvasContext.createImageData(this.canvasWidth, this.canvasHeight)
         let targetData = this.target.data
 
         const t1 = new Date().getTime()
 
-        const eqProj = projectionsMap['geoEquirectangular'].fn()
-                        .scale(100)
-                        .translate([this._canvasWidth / 2, this._canvasHeight / 2])
+        for (var y = 0, i = -1; y < this.canvasHeight; y += 1) {
+            for (var x = 0; x < this.canvasWidth; x += 1) {
+              const _x = (x / this.canvasWidth - 0.5) * 3 * (360 / Math.PI)
+              const _y = (y / this.canvasHeight - 0.5) * 3 * (360 / Math.PI)
+              i = y * (this.canvasWidth) * 4 + x * 4 - 1
 
-        for (var y = 0, i = -1; y < this._canvasHeight; ++y) {
-            for (var x = 0; x < this._canvasWidth; ++x) {
-              const _x = (x / this._canvasWidth - 0.5) * 3 * (360 / Math.PI)
-              const _y = (y / this._canvasHeight - 0.5) * 3 * (360 / Math.PI)
               var p = this.projection.invert([_x, _y])
               if (!p) continue
               let λ = p[0], φ = p[1];
@@ -70,44 +74,19 @@ export default class Index extends React.Component {
                 targetData[++i] = 128;
                 targetData[++i] = 255;  
                 continue
-                //   i += 4; continue; 
               }
-              var q = (((90 - φ) / 180 * this._canvasHeight | 0) * this._canvasWidth + ((180 + λ) / 360 * this._canvasWidth | 0) << 2)
-              if (x < 5 || y < 5 || x > this._canvasWidth - 5 || y > this._canvasHeight - 5) {
-                targetData[++i] = 0;
-                targetData[++i] = 0;
-                targetData[++i] = 255;
-                targetData[++i] = 255;  
-              } else {
+              var q = (((90 - φ) / 180 * this.canvasHeight | 0) * this.canvasWidth + ((180 + λ) / 360 * this.canvasWidth | 0) << 2)
                 targetData[++i] = this.sourceData[q];
                 targetData[++i] = this.sourceData[++q];
                 targetData[++i] = this.sourceData[++q];
                 targetData[++i] = 255;  
-              }
             }
           }        
 
         const t2 = new Date().getTime()
 
-        this.canvasContext.clearRect(0, 0, this._canvasWidth, this._canvasHeight);
+        this.canvasContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
         this.canvasContext.putImageData(this.target, 0, 0);
-
-        var projection = this.projection
-            // .translate([this._canvasWidth / 2, this._canvasHeight / 2])
-            // .scale(this._canvasWidth / (2 * Math.PI));
-
-        var geoGenerator = d3.geoPath()
-            .projection(projection)
-            .context(this.canvasContext)
-        // var geoCircle = d3.geoCircle().radius(10).precision(1);
-        var geoGraticule = d3.geoGraticule();
-        this.canvasContext.strokeStyle = '#ccc';
-        this.canvasContext.fillStyle = 'none';
-        this.canvasContext.setLineDash([1,1]);
-        // this.canvasContext.beginPath();
-        // geoGenerator(geoGraticule());
-        // this.canvasContext.stroke();      
-
 
         const t3 = new Date().getTime()
 
@@ -116,18 +95,12 @@ export default class Index extends React.Component {
     }
     updateProjection() {
         const { scale, rotateX, rotateY, rotateZ, translateX, translateY, projection } = this.state
-        //d3GeoProjection.geoAiry()
-        //d3GeoProjection.geoBonne().parallel(45)
-        //d3GeoProjection.geoEckert1()
-        //d3GeoProjection.geoPierceQuincuncial()
         const currentProjection = projectionsMap[projection]
-        // console.log('Current projection is: ', currentProjection, scale)
         this.projection = currentProjection.fn()
         if (this.projection.scale) this.projection = this.projection.scale(3 * scale)
-        if (this.projection.translate) this.projection = this.projection.translate([this._canvasWidth / 2 + this._canvasWidth * (translateX - 50) / 50, this._canvasHeight / 2 + this._canvasHeight * (translateY - 50) / 50])
+        if (this.projection.translate) this.projection = this.projection.translate([this.canvasWidth / 2 + this.canvasWidth * (translateX - 50) / 50, this.canvasHeight / 2 + this.canvasHeight * (translateY - 50) / 50])
         if (this.projection.rotate) this.projection = this.projection.rotate([rotateX, rotateY, rotateZ])
         if (this.projection.precision) this.projection = this.projection.precision(1)
-        // this.path = d3Geo.geoPath(this.projection)//.projection(this.projection)
     }
     componentDidMount() {
         this.updateProjection()
@@ -200,6 +173,15 @@ export default class Index extends React.Component {
 
         if (file) reader.readAsDataURL(file)
     }
+    onCanvasMouseDown = (evt) => {
+
+    } 
+    onCanvasMouseUp = (evt) => {
+
+    }
+    onCanvasMouseMove = (evt) => {
+        
+    }
     render() {
         const { scale, rotateX, rotateY, rotateZ, translateX, translateY, projection } = this.state
         return (
@@ -209,16 +191,19 @@ export default class Index extends React.Component {
                         <section>
                             <div {...getRootProps()}>
                                 <input {...getInputProps()} />
-                                <p>Drag 'n' drop some files here, or click to select files</p>
                                 <img 
                                     ref={this.onImageRef}
                                     onLoad={this.onImageLoad}
                                     style={{display: 'none'}}
                                 />
                                 <canvas 
-                                    width={this._canvasWidth}
-                                    height={this._canvasHeight}
+                                    width={this.canvasWidth}
+                                    height={this.canvasHeight}
                                     ref={this.onCanvasRef}
+                                    onMouseDown={this.onCanvasMouseDown}
+                                    onMouseUp={this.onCanvasMouseUp}
+                                    onMouseMove={this.onCanvasMouseMove}
+                                    className="main-canvas"
                                 >
                                 </canvas>
 
