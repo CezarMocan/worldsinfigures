afa6e4151faa6fec2c48a6abd2c581aedfc0f33d
Cezar Mocan <mocancezar@gmail.com>
2019-08-09 14:36:31 +0200

    Added some metadata

--- modules/Projections.js
+++ modules/Projections.js
@@ -1,8 +1,6 @@
 import * as d3GeoProjection from "d3-geo-projection"
 import * as d3 from 'd3'
 
-// d3GeoProjection['geoEquirectangular'] = d3.geoEquirectangular
-
 const getProjectionDisplayNameFromKey = (k) => {
     return k.substring(3).split("").reduce((acc, val, index) => {
         if (val == val.toUpperCase() && val >= 'A' && index != 0) return acc + ' ' + val
@@ -31,9 +29,6 @@ let allProjectionIds = [
 Object.keys(d3GeoProjection).forEach(p => allProjectionIds.push(p))
 
 const viableProjectionIds = allProjectionIds.filter(id => id.indexOf('Raw') == -1)
-                                            // .filter(id => !!(d3GeoProjection[id]().invert))
-
-console.log('Viable projection ids: ', viableProjectionIds)
 
 export const projectionsList = viableProjectionIds.map(p => {
     return {
---
+++ modules/ProjectionsMetadata.js
@@ -0,0 +1,130 @@
+export const projectionsMetadata = {
+    'equirectangular': {
+        flagEmoji: '🇬🇷',
+        genderEmoji: '👨',
+        year: '100 C.E.',
+        authorName: 'Marinus of Tyre'
+    },
+    'albers': {
+        flagEmoji: '🇩🇪',
+        genderEmoji: '👨',
+        year: '1805',
+        authorName: 'Heinrich C. Albers'
+    },
+    'azimuthal equal area': {
+        flagEmoji: '',
+        genderEmoji: '',
+        year: '',
+        authorName: ''
+    },
+    'azimuthal equidistant': {
+        flagEmoji: '',
+        genderEmoji: '',
+        year: '',
+        authorName: ''
+    },
+    'conic conformal': {
+        flagEmoji: '🇨🇭',
+        genderEmoji: '👨',
+        year: '1772',
+        authorName: 'Johann Heinrich Lambert'
+    },
+    'conic equal area': {
+        flagEmoji: '',
+        genderEmoji: '',
+        year: '',
+        authorName: ''
+    },
+    'equirectangular': {
+        flagEmoji: '🇬🇷',
+        genderEmoji: '👨',
+        year: '100 C.E.',
+        authorName: 'Marinus of Tyre'
+    },
+    'conic equidistant': {
+        flagEmoji: '🇬🇷',
+        genderEmoji: '👨',
+        year: '150 A.D.',
+        authorName: 'Claudius Ptolemy'
+    },
+    'equal earth': {
+        flagEmoji: '🇸🇮 🇦🇺 🇺🇸',
+        genderEmoji: '👨 👨 👨',
+        year: '2018',
+        authorName: 'Bojan Šavrič, Bernhard Jenny, Tom Patterson'
+    },
+    'gnomonic': {
+        flagEmoji: '',
+        genderEmoji: '',
+        year: '',
+        authorName: ''
+    },
+    'mercator': {
+        flagEmoji: '🇧🇪',
+        genderEmoji: '👨',
+        year: '1569',
+        authorName: 'Gerardus Mercator'
+    },
+    'natural earth': {
+        flagEmoji: '🇺🇸',
+        genderEmoji: '👨',
+        year: '2012',
+        authorName: 'Tom Patterson'
+    },
+    'orthographic': {
+        flagEmoji: '',
+        genderEmoji: '',
+        year: '',
+        authorName: ''
+    },
+    'stereographic': {
+        flagEmoji: '',
+        genderEmoji: '',
+        year: '',
+        authorName: ''
+    },
+    'transverse mercator': {
+        flagEmoji: '',
+        genderEmoji: '',
+        year: '',
+        authorName: ''
+    },
+    'airy': {
+        flagEmoji: '🇬🇧',
+        genderEmoji: '👨',
+        year: '1861',
+        authorName: 'George Biddell Airy'
+    },
+    'aitoff': {
+        flagEmoji: '🇷🇺',
+        genderEmoji: '👨',
+        year: '1889',
+        authorName: 'David Aitoff'
+    },
+    'armadillo': {
+        flagEmoji: '🇭🇺🇺🇸',
+        genderEmoji: '👨',
+        year: '',
+        authorName: 'Erwin Raisz'
+    },
+    'august': {
+        flagEmoji: '',
+        genderEmoji: '👨 👨',
+        year: '1874',
+        authorName: 'Friedrich W. O. August, G. Bellermann'
+    },
+    'baker': {
+        flagEmoji: '',
+        genderEmoji: '👨',
+        year: '1986',
+        authorName: 'J.G.P. Baker'
+    },
+    'berghaus': {
+        flagEmoji: '🇩🇪',
+        genderEmoji: '👨',
+        year: '1879',
+        authorName: 'Heinrich Berghaus'
+    }
+}
+
+export const getFlag
