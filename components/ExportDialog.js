65ca4468333118791cc862a29f9de06e3cb8711e
Cezar Mocan <mocancezar@gmail.com>
2020-12-22 02:41:46 +0200

    zipping message

--- components/ExportDialog.js
+++ components/ExportDialog.js
@@ -4,10 +4,16 @@ export default class ExportDialog extends React.Component {
     render() {
         const { currentFrame, totalFrames, canvas } = this.props
         const src = canvas.toDataURL('image/png')
+        const isZipping = (currentFrame == totalFrames)
         return (
           <div className="export-modal-container">
             <div>
-              <h1> Rendering: {parseInt(currentFrame / totalFrames * 100)}% ({currentFrame} / {totalFrames}) </h1>              
+              { !isZipping &&
+                <h1> Rendering: {parseInt(currentFrame / totalFrames * 100)}% ({currentFrame} / {totalFrames}) </h1>              
+              }
+              { isZipping &&
+                <h1> Archiving... Please wait for the browser download dialog without closing this page. </h1>              
+              }
             </div>
             <div>
               <img src={src} style={{width: '300px', height: 'auto'}}/>
